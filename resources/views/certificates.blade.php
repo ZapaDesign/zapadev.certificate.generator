137106fcbc60a3d8de93352d99520010feb68e5e
zapaDEV <zapa.poltava@gmail.com>
2022-08-14 17:36:47 +0300

    init commit

---
+++ resources/views/certificates.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Certificate list') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+{{--                <x-jet-welcome />--}}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda commodi cupiditate dolorem dolores, doloribus dolorum ea eligendi fuga illum incidunt molestiae molestias nobis numquam, quaerat recusandae repudiandae sequi tempore!
+            </div>
+        </div>
+    </div>
+</x-app-layout>
