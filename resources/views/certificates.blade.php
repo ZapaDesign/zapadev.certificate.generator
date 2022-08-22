c05638e0060527ec314f67d459a4f2caa3b6b714
Sergey Z <sergey.z@thewhitelabelagency.com>
2022-08-22 18:08:09 +0300

    Add certificates blade

--- resources/views/certificates.blade.php
+++ resources/views/certificates.blade.php
@@ -9,7 +9,122 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 {{--                <x-jet-welcome />--}}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda commodi cupiditate dolorem dolores, doloribus dolorum ea eligendi fuga illum incidunt molestiae molestias nobis numquam, quaerat recusandae repudiandae sequi tempore!
+
+
+
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                        <tr>
+                                            <th
+                                                scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >ID</th>
+                                            <th
+                                                scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >Student</th>
+
+                                            <th
+                                                scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >Level</th>
+                                            <th
+                                                scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >Start</th>
+                                            <th
+                                                scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >Finish</th>
+                                            <th
+                                                scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >Time</th>
+                                            <th
+                                                scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >Location</th>
+
+                                            <th scope="col" class="relative px-6 py-3">
+                                                <span class="sr-only">Edit</span>
+                                            </th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr v-for="photo in photos" :key="photo.id">
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div
+                                                class="text-sm text-gray-900"
+                                            >1</div>
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                Name
+                                            </div>
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                A1
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                2012-05-12
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                2015-06-25
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                155
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                New Your
+                                            </div>
+                                        </td>
+                                        <!-- ACTIONS -->
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+
+                                            <Link
+                                                class="text-indigo-600 hover:text-indigo-900 pl-3 pr-3"
+                                                :href="route('admin.photos.show', photo.id)"
+                                            >
+                                            View
+                                            </Link>
+
+                                            <Link
+                                                class="text-indigo-600 hover:text-indigo-900 pl-3 pr-3"
+                                                :href="route('admin.photos.edit', photo.id)"
+                                            >Edit</Link>
+
+                                            <JetButton
+                                                class="text-indigo-600 hover:text-indigo-900 cursor-pointer"
+                                                @click="delete_photo(photo)">
+                                                Delete
+                                            </JetButton>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
             </div>
         </div>
     </div>
